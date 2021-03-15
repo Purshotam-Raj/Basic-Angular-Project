574765ebc5b354c2bbc577c5e4564c4088d67b61
Purshotam Raj <purshotam.raj@fretron.com>
2021-03-15 18:18:09 +0530

    V-1.0

---
+++ src/app/Shopping-list/Shopping-list.component.ts
@@ -0,0 +1,14 @@
+import { style } from "@angular/animations";
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-Shopping-list',
+    templateUrl: './Shopping-list.component.html',
+    styleUrls:['./Shopping-list.component.css']
+})
+
+export class ShoppingListComponent{
+    
+    onInit(){}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Header/Header.component';
+import { ShoppingListComponent } from './Shopping-list/Shopping-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListEditComponent } from './Shopping-list/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListEditComponent
   ],
   imports: [
     BrowserModule,
